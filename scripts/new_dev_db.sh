81ef148104771c5d658a02e5d708812d1270db54
Stephan Meylan <meylan.stephan@gmail.com>
2024-04-03 15:38:00 -0400

    updated db creation for chidles_db_derived

--- scripts/new_dev_db.sh
+++ scripts/new_dev_db.sh
@@ -5,6 +5,5 @@ echo "Enforcing schema..."
 cd ../
 python manage.py makemigrations db
 python manage.py migrate db
-echo "Populating....."
-python3 -m pdb -c c manage.py populate_db --data_root /home/ubuntu/peekbank_data
-python manage.py rle_custom_migration
+#echo "Populating....."
+#python3 -m pdb -c c manage.py populate_db --data_root /home/ubuntu/peekbank_data
--- scripts/new_dev_db.sql
+++ scripts/new_dev_db.sql
@@ -1,5 +1,5 @@
-CREATE USER IF NOT EXISTS 'reader'@'%' IDENTIFIED BY 'gazeofraccoons';
-DROP DATABASE IF EXISTS peekbank_dev;
-CREATE DATABASE peekbank_dev;
-GRANT ALL PRIVILEGES ON peekbank_dev.* TO 'root'@'localhost';
-GRANT SELECT ON peekbank_dev.* TO 'reader'@'%';
+CREATE USER IF NOT EXISTS 'reader'@'%' IDENTIFIED BY 'childspeech';
+DROP DATABASE IF EXISTS childes_db_derived_dev;
+CREATE DATABASE childes_db_derived_dev;
+GRANT ALL PRIVILEGES ON childes_db_derived_dev.* TO 'root'@'localhost';
+GRANT SELECT ON childes_db_derived_dev.* TO 'reader'@'%';
